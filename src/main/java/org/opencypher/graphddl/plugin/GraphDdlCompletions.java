b54f38cfc3e98496075270ff674c0a9b894e9386
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2018-11-05 10:44:00 +0100

    Completion on keywords

---
+++ src/main/java/org/opencypher/graphddl/plugin/GraphDdlCompletions.java
@@ -0,0 +1,59 @@
+package org.opencypher.graphddl.plugin;
+
+
+import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.NotNull;
+
+public class GraphDdlCompletions extends CompletionContributor {
+    private final String[] KEYWORDS = new String[] {
+            "CATALOG",
+            "CREATE",
+            "LABEL",
+            "GRAPH",
+            "KEY",
+            "WITH",
+            "FROM",
+            "NODE",
+            "NODES",
+            "RELATIONSHIP",
+            "SET",
+            "SETS",
+            "JOIN",
+            "ON",
+            "AND",
+            "AS",
+            "SCHEMA",
+            "START",
+            "END"
+    };
+
+    private final String[] TYPES = new String[] {
+            "STRING",
+            "INTEGER",
+            "FLOAT",
+            "BOOLEAN"
+    };
+
+    public GraphDdlCompletions() {
+        extend(
+            CompletionType.BASIC,
+            PlatformPatterns.psiElement(PsiElement.class).withLanguage(GraphDdlFileType.Language.INSTANCE),
+            new CompletionProvider<CompletionParameters>() {
+                public void addCompletions(@NotNull CompletionParameters parameters,
+                                           ProcessingContext context,
+                                           @NotNull CompletionResultSet resultSet) {
+                    for (String keyword : KEYWORDS) {
+                        resultSet.addElement(LookupElementBuilder.create(keyword));
+                    }
+                    for (String type : TYPES) {
+                        resultSet.addElement(LookupElementBuilder.create(type));
+                    }
+                }
+            }
+        );
+    }
+}
